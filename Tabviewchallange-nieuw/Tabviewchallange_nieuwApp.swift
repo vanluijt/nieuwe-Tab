6a0e358ca3c4ccf395a8f430eb6f24ba28ad636f
vanluijt <vl54@ziggo.nl>
2022-08-06 11:18:58 +0200

    Initial Commit

---
+++ Tabviewchallange-nieuw/Tabviewchallange_nieuwApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tabviewchallange_nieuwApp.swift
+//  Tabviewchallange-nieuw
+//
+//  Created by P.A. van Luijt on 06/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tabviewchallange_nieuwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
