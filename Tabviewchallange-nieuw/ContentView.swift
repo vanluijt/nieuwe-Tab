c3e2593a92c59c98b9b0df4e4b29a8daaf8e6403
vanluijt <vl54@ziggo.nl>
2022-08-06 21:58:26 +0200

    Dit lijkt beter

--- Tabviewchallange-nieuw/ContentView.swift
+++ Tabviewchallange-nieuw/ContentView.swift
@@ -1,16 +1,49 @@
 //
 //  ContentView.swift
-//  Tabviewchallange-nieuw
+//  Tabviewchallenge
 //
-//  Created by P.A. van Luijt on 06/08/2022.
+//  Created by P.A. van Luijt on 05/08/2022.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var tabIndex = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        TabView(selection: $tabIndex){
+            //MARK: Tab 1
+            Text("Deze tab is \(tabIndex).")
+                .tabItem{
+                    VStack{
+                        Image(systemName:"tortoise")
+                        Text("Tab 1")
+                    }
+                }.tag(0)
+            // MARK: Tab 2
+            Button("Breng me naar tab 3"){ tabIndex = 2 }
+                .tabItem{
+                    VStack{
+                        Image(systemName:"arrow.right.square")
+                        Text("Tab 2")
+                    }
+                }.tag(1)
+            // MARK: Tab 3
+            List {
+                ForEach(0..<100){
+                    _ in Text("Dit is tab 3")
+                }
+            }
+            .tabItem{
+                VStack{
+                    Image(systemName: "hands.clap")
+                    Text("Tab 3")
+                }
+            }.tag(2)
+        }
+        
+        
     }
 }
 
